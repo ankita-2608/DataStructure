6d1eb37d50ff57649d3b7e69ae9d0592df36e797
ankita-2608 <118102456+ankita-2608@users.noreply.github.com>
2023-01-08 18:33:36 +0530

    Create inOrderPredecessor.cpp

---
+++ inOrderPredecessor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+//the previous node in Inorder traversal of the Binary Tree
+Node* inOrderPredecessor(Node* root) {
+    root = root->left;
+    while(root->right) {
+        root = root->right;
+    }
+    return root;
+}
+
+int main()
+{
+
+    Node *p = new Node(5);
+    Node *p1 = new Node(3);
+    Node *p2 = new Node(6);
+    Node *p3 = new Node(2);
+    Node *p4 = new Node(4);
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    return 0;
+}
